3464d0404a2a8a5a69ee4a56cbeabdfd210f3a1e
tomrajda <eu.crafciarze@gmail.com>
2023-01-07 15:56:11 +0100

    Update 07.01.2023

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "https://my.api.mockaroo.com/Airports.json?key=3d629ea0";
+xmlhttp.open("GET",url,true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        var data = JSON.parse(this.responseText);
+        //console.log(data)
+        Airport_Name = data.map(function(elem){
+            return elem.Airport_Name;
+        })
+        Airport_Year_Visits = data.map(function(elem){
+            return elem.Airport_Year_Visits;
+        })        
+        //console.log(Airport_Year_Visits)
+
+        const ctx = document.getElementById('canvas').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: Airport_Name.slice(0, 30),
+                datasets: [{
+                    label: 'Total passegner traffic',
+                    data: Airport_Year_Visits.slice(0, 30),
+                    backgroundColor: ["#ff335e", "#ff338e", "#ff332e", "#ff337e"]
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Total passenger traffic at selected airports worldwide in 2022',
+                        font: {
+                            size: 24,
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ js/custom2.js
@@ -0,0 +1,46 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "https://my.api.mockaroo.com/Airports.json?key=3d629ea0";
+xmlhttp.open("GET",url,true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        var data = JSON.parse(this.responseText);
+        //console.log(data)
+        Airport_Name = data.map(function(elem){
+            return elem.Airport_Name;
+        })
+        Airport_Elevation = data.map(function(elem){
+            return elem.Airport_Elevation
+        })        
+        //console.log(Airport_Year_Visits)
+
+        const ctx = document.getElementById('canvas2').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: Airport_Name.slice(0, 30),
+                datasets: [{
+                    label: 'Height above sea level',
+                    data: Airport_Elevation.slice(0, 30),
+                    backgroundColor: "#ff335e",
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Height above sea level of the selected airports',
+                        font: {
+                            size: 24,
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
